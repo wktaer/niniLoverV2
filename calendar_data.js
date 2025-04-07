cd89b33f77f990ac31a5b0fbf5e69711fea776de
wktaer <98446388+wktaer@users.noreply.github.com>
2025-04-07 17:59:24 -0400

    version2

--- calendar_data.js
+++ calendar_data.js
@@ -4,14 +4,38 @@ const specialNotes = {
     '2025': {
         // Abril 2025
         '4': {
-            1: { phrase: "Cada sonrisa tuya ilumina mi día. ✨", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
-            7: { phrase: "Tu amor es mi mayor tesoro. 💝", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
-            8: { phrase: "frase del dia 8 ✨", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
-            9: { phrase: "frase del dia 9 🌸", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
-            10: { phrase: "Cada día te quiero más-10. 💖", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
-            25: { phrase: "Mi corazón late por ti. 💓", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
-            29: { phrase: "Contigo cada momento es especial. 💕", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" }
+            1: { phrase: "Eres mi lugar favorito en todo el mundo. 💖", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            2: { phrase: "No sé qué hice bien en la vida pa' merecerte, pero gracias Diosito. 😍", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            3: { phrase: "Me encantas... en todas las versiones tuyas. 👀🔥", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            4: { phrase: "A veces pienso en ti y me sale una sonrisita weona solo. 💕", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            5: { phrase: "Diosito me premió mandándome a la mujer más linda. 😘", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            6: { phrase: "Mi plan siempre va a ser hacerte feliz, hasta viejitos. 🧓🏻👵🏻", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            7: { phrase: "Eres más rica que dormir tapadito en invierno. 🥵❤️", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            8: { phrase: "Tus besos son adictivos... necesito mi dosis diaria. 😘🔥", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            9: { phrase: "No hay nadie como tú... eres mi persona favorita. ✨", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            10: { phrase: "Contigo se me pasa rápido el día... pero lento cuando no estás. 💓", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            11: { phrase: "No es por nada... pero qué mina más rica me conseguí. 🔥", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            12: { phrase: "Cada mensaje tuyo me hace sonreír como tonto enamorado. 😍", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            13: { phrase: "Te pienso más de lo que debería... pero me encanta. 😏", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            14: { phrase: "Si pudiera pedir un deseo... sería tenerte aquí abrazadita. 🤗", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            15: { phrase: "Eres mi debilidad... y mi mejor fortuna. ❤️", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            16: { phrase: "Hoy amanecí con más ganas de ti que de desayuno. 👅🔥", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            17: { phrase: "Nunca voy a cansarme de ti, nunca. 💕", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            18: { phrase: "Oye mi niña linda... te amo más de lo que crees. 🥺", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            19: { phrase: "Te quiero pa' mi, pa' siempre, pa' todo. 💍", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            20: { phrase: "Me haces feliz de una manera que nadie más puede. 💖", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            21: { phrase: "Si ser cursi es delito... que me metan preso por ti. 🥰", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            22: { phrase: "Hoy ando con ganas de abrazarte hasta quedarme dormido. 💤", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            23: { phrase: "No hay otra como tú... ni quiero buscar. 💗", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            24: { phrase: "Estoy totalmente enviciado a tu amor. 😍🔥", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            25: { phrase: "Qué suerte la mía tener a una mujer tan bacán y hermosa. 😎❤️", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            26: { phrase: "Si tú eres un sueño... que no me despierten nunca. ✨", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            27: { phrase: "Estoy tan enamorado de ti que ya me acostumbré a extrañarte. 🥺", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            28: { phrase: "Mis ganas de ti no tienen días libres. 🔥👀", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            29: { phrase: "Eres el mejor motivo que tengo pa' ser mejor cada día. 💖", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" },
+            30: { phrase: "Gracias por existir mi amorcito lindo, te amo infinito. ♾️❤️", img: "https://media.tenor.com/ZUmTKQanA2UAAAAd/pucca.gif" }
         }
+        
     },
     '2024': {
         // Enero 2024
